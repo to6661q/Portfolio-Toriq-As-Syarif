d16cd639fc2537943b1889678ae5f4a356fff6e2
Toriq As Syarif <50982479+to6661q@users.noreply.github.com>
2026-05-10 09:45:13 +0700

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,7 +1,7 @@
-// Gunakan CDN Supabase agar tidak perlu install npm (cocok untuk GitHub Pages)
+// Gunakan CDN Supabase untuk GitHub Pages
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const supabaseUrl = 'https://relosxezemvjiilnhrrp.supabase.co'; // Ganti dengan URL milikmu
+const supabaseUrl = 'https://relosxezemvjiilnhrrp.supabase.co'; 
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlbG9zeGV6ZW12amlpbG5ocnJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzNDI1NDIsImV4cCI6MjA5MzkxODU0Mn0.RRTm3L9J9JpBCT123-vN3MU8ju0FgUnqNGIFf7dbrEM'; // Ganti dengan Anon Key milikmu
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
