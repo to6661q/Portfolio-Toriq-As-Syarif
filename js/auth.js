a7f196161b471235f363f6efc09beb85487d0881
Toriq As Syarif <50982479+to6661q@users.noreply.github.com>
2026-05-13 11:00:05 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,35 +1,28 @@
 import { supabase } from './supabase-config.js';
-
 const loginForm = document.getElementById('login-form');
 const messageDiv = document.getElementById('message');
-
 loginForm.addEventListener('submit', async (e) => {
     e.preventDefault();
-    
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const btn = document.getElementById('login-btn');
-
     btn.innerText = 'Authenticating...';
     btn.disabled = true;
-
-    // Proses Login ke Supabase
+    // LOGIN PROCESS TO SUPABASE
     const { data, error } = await supabase.auth.signInWithPassword({
         email: email,
         password: password,
     });
-
     if (error) {
         messageDiv.innerText = "Login gagal: " + error.message;
         btn.innerText = 'Sign In';
         btn.disabled = false;
     } else {
-        // Berhasil! Arahkan ke Dashboard Admin
+        // SUCCESS! LOGIN TO DASHBOARD ADMIN
         window.location.href = 'admin.html';
     }
 });
-
-// Fungsi Logout (Bisa dipanggil dari tombol logout di admin.html)
+// LOGOUT FUNCTION (Can be called from the logout button in admin.html)
 export async function handleLogout() {
     const { error } = await supabase.auth.signOut();
     window.location.href = 'login.html';
