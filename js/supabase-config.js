80db075f80bd1cf35fa0ef1744088799c9cac56b
Toriq As Syarif <50982479+to6661q@users.noreply.github.com>
2026-05-13 10:33:14 +0700

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,7 +1,6 @@
-// Gunakan CDN Supabase untuk GitHub Pages
+// USING CDN SUPABASE FOR GIHUB PAGES
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-const supabaseUrl = 'https://relosxezemvjiilnhrrp.supabase.co'; 
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlbG9zeGV6ZW12amlpbG5ocnJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzNDI1NDIsImV4cCI6MjA5MzkxODU0Mn0.RRTm3L9J9JpBCT123-vN3MU8ju0FgUnqNGIFf7dbrEM'; // Ganti dengan Anon Key milikmu
-
+const supabaseUrl = 'https://relosxezemvjiilnhrrp.supabase.co';
+ // ANON KEY
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlbG9zeGV6ZW12amlpbG5ocnJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzNDI1NDIsImV4cCI6MjA5MzkxODU0Mn0.RRTm3L9J9JpBCT123-vN3MU8ju0FgUnqNGIFf7dbrEM';
 export const supabase = createClient(supabaseUrl, supabaseKey);
